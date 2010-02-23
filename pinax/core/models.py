59dc4472875c5425d27866621a9234706e72b34a
Justin Quick <justquick@gmail.com>
2010-02-23 14:48:09 -0500

    fixes ticket #266, connects a post_syncdb signal to update current site's name from settings.SITE_NAME if it is not already set in the db

--- pinax/core/models.py
+++ pinax/core/models.py
@@ -1,1 +1,20 @@
-# This file is intentionally left blank to allow Django to load the app.
+from django.conf import settings
+from django.db.models.signals import post_syncdb
+from django.contrib.sites.models import Site
+
+def site_name_handler(created_models, **kwargs):
+    if Site in created_models:
+        try:
+            site = Site.objects.get_current()
+        except Site.DoesNotExist:
+            return
+        if hasattr(settings, 'SITE_NAME'):
+            if site.name == settings.SITE_NAME:
+                # If the name is already set, dont re-save it again
+                return
+            site.name = settings.SITE_NAME
+        if hasattr(settings, 'SITE_DOMAIN'):
+            site.domain = settings.SITE_DOMAIN
+        site.save()
+        
+post_syncdb.connect(site_name_handler)
